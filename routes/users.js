5ac4797f0cc6cbbe4091965a158d15f7fb3a1b3a
MarioMarkov <mario11septemvri@gmail.com>
2021-03-01 11:42:50 +0200

    User Login with JWT done

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { body, validationResult, check } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 
 const User = require('../models/User')
@@ -42,7 +44,20 @@ router.post('/',[
         
         await user.save();
 
-        res.send('User saved')
+      const payload={
+          user:{
+              id:user.id
+          }
+      }
+
+      jwt.sign(payload, config.get('jwtSecret'),{
+          expiresIn: 36000
+      },(err,token)=>{
+          if(err){
+              throw err;
+          }
+          res.json({token});
+      });
 
     } catch (err) {
         console.log(err.message);
