1f3f4d0e1e44c31683169ee21391c1129092b65b
MarioMarkov <mario11septemvri@gmail.com>
2021-03-04 12:57:50 +0200

    Added Front-end with React

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+ const About = () => {
+    return (
+        <div>
+            <h1>Bout his App</h1>
+            <p className='py-1'>
+                Full stack React App for keeping contacts
+            </p>
+            <p className='bg-dark p'>
+                <strong>1.0.0</strong>
+            </p>
+        </div>
+    )
+}
+
+export default About;
